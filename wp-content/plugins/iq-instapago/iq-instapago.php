70f6f0845a9ca96f161bd134901f04d524760622
jnka070 <54738562+jnka070@users.noreply.github.com>
2025-02-22 19:18:17 -0500

    Feat: update of text-domain

---
+++ wp-content/plugins/iq-instapago/iq-instapago.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * 
+ * @link              https://iqtsystems.com/
+ * @since             1.0.0
+ * @package           iq-instapago
+ * @author     		  IQ <contact@iqtsystems.com>
+ * 
+ * @wordpress-plugin
+ * Plugin Name:       IQ Instapago
+ * Plugin URI:        https://iqtsystems.com/
+ * Description:       Plugin de pagos para IQ
+ * Version:           1.0.0
+ * Author:            IQ
+ * Author URI:        https://iqtsystems.com/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       iq-instapago
+ * Domain Path:       /languages
+ * 
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+    die;
+}
+
+define( 'IQ_INSTAPAGO_VERSION', '1.0.0' );
+
+function activate_iq_instapago() {
+    require_once plugin_dir_path( __FILE__ ) . 'includes/class-iq-instapago-activator.php';
+    IQ_Instapago_Activator::activate();
+}
+
+function deactivate_iq_instapago() {
+    require_once plugin_dir_path( __FILE__ ) . 'includes/class-iq-instapago-deactivator.php';
+    IQ_Instapago_Deactivator::deactivate();
+}
+
+register_activation_hook( __FILE__, 'activate_iq_instapago' );
+register_deactivation_hook( __FILE__, 'deactivate_iq_instapago' );
+
+require plugin_dir_path( __FILE__ ) . 'includes/class-iq-instapago.php';
+
+function run_iq_instapago() {
+    $plugin = new IQ_Instapago();
+    $plugin->run();
+}
+
+run_iq_instapago();
--- wp-content/plugins/qaroni-pagos/includes/class-qaroni-pagos-activator.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class Qaroni_Pagos_Activator {
-
-    public static function activate() {
-         
-    }
-}
